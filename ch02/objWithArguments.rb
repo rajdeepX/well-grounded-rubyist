c26a5749c822831c886d7edaa00d8fe334d951f3
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-01 15:05:01 +0530

    Create obj with arguments

---
+++ ch02/objWithArguments.rb
@@ -0,0 +1,14 @@
+obj = Object.new
+
+def obj.add(a,b)
+c = a + b
+print c 
+puts " is the added value."
+end
+
+print "Type your first num: "
+arg1 = gets.to_i
+print "Type your second num: "
+arg2 = gets.to_i
+
+obj.add(arg1, arg2)
