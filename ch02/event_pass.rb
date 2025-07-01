946aa887245e7f511ccabc957bc8f758713328ee
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-01 15:48:57 +0530

    boolean in methods

--- ch02/event_pass.rb
+++ ch02/event_pass.rb
@@ -16,6 +16,13 @@ def pass.price
 199.9
 end
 
+def pass.availability?
+true
+end
 
 puts "This is a #{pass.event} pass,that is to be hosted in #{pass.venue}.\nGrab yours just for Rs.#{"%.2f" %pass.price}. \nRemember the date - #{pass.date}."
 
+if (pass.availability?)
+puts "Hurry up while it's available!"
+else puts "Unfortunately, we are sold!"
+end
