ec87929acd94a7eafe91ed6c20b3ca54c1bb3e09
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-01 22:46:30 +0530

    Update obj_optional_arguments.rb

--- ch02/obj_optional_arguments.rb
+++ ch02/obj_optional_arguments.rb
@@ -2,7 +2,7 @@ def more_arg(x,y,*z)
     puts "This is an example of required and optional args for a method."
     puts "The x:#{x} and y:#{y} are required"
     puts "But the z:#{z} can be any number of args coz of the * infront of it"
-    puts "The astrics '*' lets you add n number of args"
+    puts "The astrics '*', or splat arg, lets you add n number of args"
     puts "This will then be displayed as an array"
     puts "We need to use 'p' to print it otherwise the args will print in a new line"
     p x,y,z
