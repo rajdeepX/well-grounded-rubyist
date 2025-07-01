8733a41b7931fe035265cba2588e0401ffa4ba2f
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-01 17:35:21 +0530

    Create obj_send_method.rb

---
+++ ch02/obj_send_method.rb
@@ -0,0 +1,26 @@
+pass = Object.new
+
+def pass.event
+"Hackathon-28"
+end
+
+def pass.venue
+"Eden Garden"
+end
+
+def pass.date
+"10/10/10"
+end
+
+def pass.price
+199.9
+end
+
+print "What do you want to know about the pass (event, venue, date or price) : "
+req = gets.chomp
+
+if pass.respond_to?(req)
+	puts pass.send(req)
+else puts "Info not available!"
+end
+
