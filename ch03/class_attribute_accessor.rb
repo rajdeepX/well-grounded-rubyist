d259cabe66ecd32ad879a2b2ef649c1319b143d7
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-02 18:12:55 +0530

    Create class_attribute_accessor.rb

---
+++ ch03/class_attribute_accessor.rb
@@ -0,0 +1,25 @@
+class Ticket
+    attr_reader :venue, :date
+    # attr_writer :price
+    attr_accessor :price
+
+    def initialize(venue, date)
+        @venue = venue
+        @date = date
+    end
+
+    def to_string
+        <<~END
+            VENUE: #{venue}
+            DATE: #{date}
+            PRICE: #{price}
+        END
+    end
+end
+
+e2 = Ticket.new("Times Sq.", "01-02-03")
+puts e2.date
+puts e2.venue
+
+e2.price = 1500
+puts e2.to_string
