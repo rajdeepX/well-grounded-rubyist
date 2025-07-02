84c87d6f647fc41f30351ffb2fe1d124dfbd4f24
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-02 17:02:00 +0530

    Update initialize_state.rb

--- ch03/initialize_state.rb
+++ ch03/initialize_state.rb
@@ -12,9 +12,21 @@ class Ticket
         @date
     end
      
+    def price=(price)
+        @price = price
+    end
+
+    def price
+        @price
+    end
+
 end
 
- e2 = Ticket.new("Times Sq.", "01-02-03")
- puts e2.date
- puts e2.venue
+e2 = Ticket.new("Times Sq.", "01-02-03")
+puts e2.date
+puts e2.venue
+
+e2.price = 1500
+puts e2.price
+
 
