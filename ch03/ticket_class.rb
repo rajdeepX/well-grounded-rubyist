2a1ba2d8a58b475f4e143fd9175e13a2a41ae7a2
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2025-07-02 15:31:13 +0530

    instance variable

---
+++ ch03/ticket_class.rb
@@ -0,0 +1,15 @@
+class Ticket
+    def add_event(e)
+     @event = e
+    end
+ 
+    def event_name
+     @event
+    end
+     
+ end
+ 
+ 
+ e1 = Ticket.new
+ e1.add_event("F1")
+ puts e1.event_name
